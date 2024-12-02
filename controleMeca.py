e47db432ccc2985146603ae8b8a28503de2eeba6
loris_lent <62310830+le-loris@users.noreply.github.com>
2024-12-02 16:45:04 +0100

    Passage en classe

--- controleMeca.py
+++ controleMeca.py
@@ -1,33 +1,54 @@
 import RPi.GPIO as GPIO
-inmport picamera2 as Picamera2
 import  time
 
-DIR = 23
-PWM = 18
 
-DIR_STATE = True
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(PWM, GPIO.OUT)
-GPIO.setup(DIR, GPIO.OUT)
-
-
-
-pwm = GPIO.PWM(PWM, 100)
-pwm.start(0)
-
-try:
-    while True:
-        for i in range(0, 101, 1):
-            pwm.ChangeDutyCycle(i)
-            time.sleep(0.02)
-        for i in range(100, -1, -1):
-            pwm.ChangeDutyCycle(i)
-            time.sleep(0.02)
-        DIR_STATE = not DIR_STATE
-        GPIO.output(DIR, DIR_STATE)
-        print("Loop!")
-except KeyboardInterrupt:
-    pwm.stop()
-    GPIO.cleanup()
+class controle_vehicule():
+    def __init__(self, DIR_pin, PWM_pin):
+        self.dir_pin = DIR_pin
+        self.pwm_pin = PWM_pin
         
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
+        GPIO.setup(self.dir_pin, GPIO.OUT)
+
+        self.dir = True
+        self.setDir(self.dir)
+
+        self.pwmValue = 0
+        self.pwm = GPIO.PWM(PWM_pin, 100)
+        self.pwm.start(self.pwmValue)
+
+    def setAccel(self, value):
+        if(self.pwmValue < 3):
+            self.pwm.ChangeDutyCycle(100)
+            time.sleep(0.02)
+        
+        self.pwmValue = value
+        self.pwm.ChangeDutyCycle(self.pwmValue)
+
+    def setDir(self, value):
+        self.dir = value
+        GPIO.output(self.dir_pin, self.dir)
+    
+    def changeDir(self):
+        self.setDir(not self.dir)
+
+    def finish(self):
+        self.pwm.stop()
+        GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    control = controle_vehicule()
+    try:
+        while True:
+            for i in range(0, 101, 1):
+                control.setAccel(i)
+                time.sleep(0.02)
+            for i in range(100, -1, -1):
+                control.setAccel(i)
+                time.sleep(0.02)
+            control.changeDir()
+            print("Loop!")
+    except KeyboardInterrupt:
+        control.finish()
